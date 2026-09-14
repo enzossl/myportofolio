4580a7ea4c0dd431de80f026dea77a017ce3234e
enzossl <enzo.susilo@ui.ac.id>
2026-09-14 19:55:05 +0700

    chore: mendaftarkan model ke admin dan mengatur CSRF trusted origins untuk deployment

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Experience, Achievement
+
+admin.site.register("Experience")
+admin.site.register("Achievement")
--- portofolio/settings.py
+++ portofolio/settings.py
@@ -34,6 +34,13 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1","enzo-susilo-myportofolio.pws.cs.ui.ac
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://enzo-susilo-myportofolio.pws.cs.ui.ac.id",
+]
+
+CSRF_COOKIE_SECURE    = True
+SESSION_COOKIE_SECURE = True
+
 # Application definition
 
 INSTALLED_APPS = [
