d97b94621da2f4b66391e18e2b535a99de17e40a
enzossl <enzo.susilo@ui.ac.id>
2026-09-19 17:22:16 +0700

    feat: menambahkan  urls routing untuk fitur experience

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_achievements, create_project, show_projects, get_projects_json, delete_project
+from main.views import show_main, show_experience, show_achievements, create_project, show_projects, get_projects_json, delete_project, create_experience, get_experience_json, delete_experience, update_experience 
 
 app_name = "main"
 
@@ -12,4 +12,8 @@ urlpatterns = [
     path("projects/", show_projects, name="show_projects"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/update/", update_experience, name="update_experience"),
+    path("experience/<uuid:experience_id>/delete/",delete_experience, name="delete_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
 ]
