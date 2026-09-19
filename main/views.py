f9dadb68897d6a1b6d2343bd9193d81a853423d2
enzossl <enzo.susilo@ui.ac.id>
2026-09-19 17:23:44 +0700

    feat: menambahkan CRUD dan JSON views untuk Experience

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from main.forms import ProjectForm
+from main.forms import ProjectForm, ExperienceForm
 
 def show_main(request):
     context = {
@@ -19,12 +19,7 @@ def show_main(request):
     return render(request, "index.html", context)
 
 
-def show_experience(request):
-    context = {
-        "name": "Enzo Susilo",
-        "experience_list": Experience.objects.all(),
-    }
-    return render(request, "experience.html", context)
+
 
 def show_achievements(request):
     context = {
@@ -58,7 +53,7 @@ def show_projects(request):
     title_query = request.GET.get("title", "").strip()
 
     context = {
-        "name": "Burhan",
+        "name": "Enzo Susilo",
         "project_list": projects,
         "title_query": title_query,
     }
@@ -83,3 +78,73 @@ def delete_project(request, project_id):
         return redirect("main:show_projects")
 
     return redirect("main:show_projects")
+
+#untuk experience 
+
+def show_experience(request):
+    json_response = get_experience_json(request)
+    
+    experiences = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    experiences = [experience.object for experience in experiences]
+    title_query = request.GET.get("title", "").strip()
+
+    context = {
+        "name": "Enzo Susilo",
+        "experience_list": experiences,
+        "title_query": title_query,
+    }
+    return render(request, "experience.html", context)
+
+def get_experience_json(request):
+    title_query = request.GET.get("title", "").strip()
+    experiences = Experience.objects.all()
+
+    if title_query:
+        experiences = experiences.filter(title__icontains=title_query)
+
+    experiences_json = serializers.serialize("json", experiences)
+    return HttpResponse(experiences_json, content_type="application/json")
+
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience baru berhasil ditambahkan!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Enzo Susilo",
+        "form": form,
+    }
+    return render(request, "experiences_form.html", context)
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience berhasil dihapus!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
+
+def update_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+    # instance=experience memastikan form terisi dengan data yang sudah ada
+    form = ExperienceForm(request.POST or None, instance=experience)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience berhasil diperbarui!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Enzo Susilo",
+        "form": form,
+    }
+    # Kita bisa menggunakan template form yang sama dengan fungsi create
+    return render(request, "experiences_form.html", context)
